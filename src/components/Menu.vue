0981a1aefa26c3850d7805dad90f46a9e2a24159
localhousee <ananditodwiky666@gmail.com>
2022-10-13 17:19:01 +0700

    make a layout for main menu

--- src/components/Menu.vue
+++ src/components/Menu.vue
@@ -1,20 +1,18 @@
 <script setup lang="ts">
+import Main from "@/layouts/Main.vue";
 </script>
 
 <template>
-  <div class="flex justify-center">
-    <div class="w-full">
-      <p class="text-pink-900/80 text-4xl lg:text-6xl text-center font-bold pt-24 lg:pt-16 pb-16">Math for <br /> Everyone</p>
-      <div class="flex justify-center w-full">
-        <div class="flex flex-col justify-center mx-auto w-full px-24 lg:px-0 lg:w-[12rem] py-6 space-y-4">
-          <button class="py-2 lg:py-4 lg:font-bold bg-transparent hover:bg-pink-300 text-pink-900 w-full rounded-full border-2 border-transparent hover:border-pink-900">Start</button>
-          <button class="py-2 lg:py-4 lg:font-bold bg-transparent hover:bg-pink-300 text-pink-900 w-full rounded-full border-2 border-transparent hover:border-pink-900">Help</button>
-          <button class="py-2 lg:py-4 lg:font-bold bg-transparent hover:bg-pink-300 text-pink-900 w-full rounded-full border-2 border-transparent hover:border-pink-900">About</button>
-        </div>
+  <Main title="Math for <br /> Everyone">
+    <div class="flex justify-center w-full">
+      <div class="flex flex-col justify-center mx-auto w-full px-24 lg:px-0 lg:w-[12rem] py-6 space-y-4">
+        <button
+          class="py-2 lg:py-4 lg:font-bold bg-transparent hover:bg-pink-300 text-pink-900 w-full rounded-full border-2 border-transparent hover:border-pink-900">Start</button>
+        <button
+          class="py-2 lg:py-4 lg:font-bold bg-transparent hover:bg-pink-300 text-pink-900 w-full rounded-full border-2 border-transparent hover:border-pink-900">Help</button>
+        <button
+          class="py-2 lg:py-4 lg:font-bold bg-transparent hover:bg-pink-300 text-pink-900 w-full rounded-full border-2 border-transparent hover:border-pink-900">About</button>
       </div>
     </div>
-    <div class="hidden lg:flex w-full items-center h-screen">
-      <img src="calculator.png" alt="calculator" class="w-full h-full overflow-hidden">
-    </div>
-  </div>
+  </Main>
 </template>
---
+++ src/layouts/Main.vue
@@ -0,0 +1,16 @@
+<script setup lang="ts">
+defineProps({
+  "title": String
+})
+</script>
+<template>
+  <div class="flex justify-center">
+    <div class="w-full">
+      <p class="text-pink-900/80 text-4xl lg:text-6xl text-center font-bold pt-24 lg:pt-16 pb-16" v-html="title" />
+      <slot />
+    </div>
+    <div class="hidden lg:flex w-full items-center h-screen">
+      <img src="calculator.png" alt="calculator" class="w-full h-full overflow-hidden">
+    </div>
+  </div>
+</template>
