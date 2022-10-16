638d8756579eace314c176cd5f8a984e531ccbd2
localhousee <ananditodwiky666@gmail.com>
2022-10-16 19:24:49 +0700

    remove error 404 on import image

--- src/App.vue
+++ src/App.vue
@@ -2,7 +2,7 @@
 </script>
 
 <template>
-  <div class="h-screen bg-cover bg-fixed bg-no-repeat bg-bottom" style="background-image:url(background.jpg)">
+  <div class="h-screen bg-cover bg-fixed bg-no-repeat bg-bottom" style="background-image:url('/background.jpg')">
     <router-view />
   </div>
 </template>
