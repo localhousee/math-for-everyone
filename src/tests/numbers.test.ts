d42750405067c27bd91f913eba1e2610fa62135f
localhousee <ananditodwiky666@gmail.com>
2022-10-17 02:02:45 +0700

    update numbers test

--- src/tests/numbers.test.ts
+++ src/tests/numbers.test.ts
@@ -1,18 +1,16 @@
-import { describe, it, expect } from "vitest";
+import { it, expect } from "vitest";
 import number from "../utilities/numbers";
 
-describe("type number", () => {
-  it("will generate whole number below or equal to limit parameter", () => {
-    let { question, answer } = number("whole number", 50);
-    expect(parseInt(answer)).to.be.below(50);
-    expect(parseInt(answer)).to.be.least(1);
-    expect(Number.isSafeInteger(parseInt(answer))).toBe(true);
-  });
+it("will generate whole number below or equal to limit parameter", () => {
+  let { question, answer } = number("whole number", 50);
+  expect(parseInt(answer)).to.be.below(50);
+  expect(parseInt(answer)).to.be.least(1);
+  expect(Number.isSafeInteger(parseInt(answer))).toBe(true);
+});
 
-  it("result of halves, thirds, and fourths are whole number", () => {
-    let { question, answer } = number("1/2, 1/3, 1/4", 100);
-    expect(parseInt(answer)).to.be.below(100);
-    expect(parseInt(answer)).to.be.least(1);
-    expect(Number.isSafeInteger(parseInt(answer))).toBe(true);
-  });
+it("result of halves, thirds, and fourths are whole number", () => {
+  let { question, answer } = number("1/2, 1/3, 1/4", 100);
+  expect(parseInt(answer)).to.be.below(100);
+  expect(parseInt(answer)).to.be.least(1);
+  expect(Number.isSafeInteger(parseInt(answer))).toBe(true);
 });
