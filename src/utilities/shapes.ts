c0442ee68e3269aa4e2456549850628dc87e530f
localhousee <ananditodwiky666@gmail.com>
2022-10-19 16:31:50 +0700

    big update on shapes

--- src/utilities/shapes.ts
+++ src/utilities/shapes.ts
@@ -2,7 +2,7 @@ import { getRandomNumber, getRandomBoolean, questionFormat } from "./helpers";
 
 interface shape {
   name: string;
-  properties: string[];
+  properties?: string[];
   symmetry?: number | string;
 };
 const twoDimentional: shape[] = [
@@ -80,89 +80,51 @@ const twoDimentional: shape[] = [
 const threeDimentional: shape[] = [
   {
     name: "Cube",
-    properties: [
-      "Faces : 6",
-      "Edges: 12",
-      "Vertices: 8",
-      "All sides are same"
-    ]
   },
   {
     name: "Rectangle Prism",
-    properties: [
-      "Faces : 6",
-      "Edges: 12",
-      "Vertices: 8",
-    ]
   },
   {
     name: "Sphere",
-    properties: [
-      "Curved face: 1",
-      "Edges: 0",
-      "Vertices: 0",
-    ]
   },
   {
     name: "Cone",
-    properties: [
-      "Flat face: 1",
-      "Curved face: 1",
-      "Edges: 1",
-      "Vertices: 1",
-    ]
   },
   {
     name: "Cylinder",
-    properties: [
-      "Flat face: 2",
-      "Curved face: 1",
-      "Edges: 2",
-      "Vertices: 0",
-    ]
   }
 ];
 
-let answer = "";
-let question = "";
-const is2D = getRandomBoolean();
-let arr: shape[] = [];
-
-if (is2D) arr = twoDimentional;
-else arr = threeDimentional;
-
 const shapes = (type: string): Result => {
+  let answer = "";
+  let question = "";
   if (type.includes("introduction")) ({ question, answer } = introduction());
-  else if (type.includes("characteristic")) {
-    if (type.includes("advanced")) ({ question, answer } = characteristic(true));
-    else ({ question, answer } = characteristic());
-  }
+  else if (type.includes("characteristic")) ({ question, answer } = characteristic());
   else if (type.includes("symmetry")) ({ question, answer } = symmetry());
   else if (type.includes("angles")) ({ question, answer } = angles());
   return { question, answer };
 }
 
 const introduction = (): Result => {
-  answer = arr[getRandomNumber(0, arr.length - 1)].name.toLowerCase().replace(" ", "-");
-  const question = questionFormat(`<img src="/dimentional-figure/${answer}.png" alt="image" class="w-1/2 h-1/2 mx-auto" />`);
+  const is2D = getRandomBoolean();
+  let arr = [];
+  if (is2D) arr = twoDimentional;
+  else arr = threeDimentional;
+
+  const answer = arr[getRandomNumber(0, arr.length - 1)].name.toLowerCase().replace(" ", "-");
+  const question = questionFormat(`<img src="/dimentional-figure/${answer}.png" alt="image" class="w-1/4 h-1/4 mx-auto" />`);
   return { question, answer };
 }
-const characteristic = (advanced?: boolean): Result => {
-  if (advanced) {
-    const chosenShape = twoDimentional[getRandomNumber(0, twoDimentional.length - 1)];
-    question = questionFormat(`What shape is it based on this properties? <br/> ${chosenShape.properties.join("<br />")}`);
-    answer = chosenShape.name.toLowerCase();
-  } else {
-    const chosenShape = arr[getRandomNumber(0, arr.length - 1)];
-    question = questionFormat(`What shape is it based on this properties? <br/> ${chosenShape.properties.join("<br />")}`);
-    answer = chosenShape.name.toLowerCase();
-  }
+const characteristic = (): Result => {
+  const shape = twoDimentional[getRandomNumber(0, twoDimentional.length - 1)];
+  const question = questionFormat(`What shape is it based on this properties?<br/> ${shape.properties!.join("<br />")}`);
+  const answer = shape.name.toLowerCase();
   return { question, answer };
 }
 const symmetry = (): Result => {
-  const chosenShape = arr[getRandomNumber(0, arr.length - 1)];
-  question = questionFormat(`How much symmetry in ${chosenShape.name}?`);
-  answer = chosenShape.symmetry!.toString();
+  const shape = twoDimentional[getRandomNumber(0, twoDimentional.length - 1)];
+  const question = questionFormat(`How much symmetry in ${shape.name}?`);
+  const answer = shape.symmetry!.toString();
   return { question, answer };
 }
 const angles = (): Result => {
