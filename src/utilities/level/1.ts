53cbbd1a57df148bdffed2fff601d6050356a25e
localhousee <ananditodwiky666@gmail.com>
2022-11-05 18:35:02 +0700

    refactor file for level 1

--- src/utilities/level/1.ts
+++ src/utilities/level/1.ts
@@ -1,7 +1,13 @@
 import {
-  getRandomNumber, questionFormat, getRandomBoolean,
-  twoDimentional, threeDimentional,
-  lengths, weights, lengthsOfTime,
+  getRandomNumber,
+  questionFormat,
+  getRandomBoolean,
+  imageFormat,
+  twoDimentional,
+  threeDimentional,
+  lengths,
+  weights,
+  lengthsOfTime,
 } from "../helpers";
 
 export const level1 = [
@@ -11,83 +17,81 @@ export const level1 = [
   "Comparing length, weight, and time",
 ];
 
-export const introductionToWholeNumberUntil50 = (): Result => {
-  const answer = getRandomNumber(5, 50);
+export function introductionToWholeNumberUntil50(): Result {
+  const number = getRandomNumber(5, 50);
   const step = getRandomNumber(1, 4);
   const isAbove = getRandomBoolean();
-  const question = questionFormat(`What's ${step} numbers ${isAbove ? 'after' : 'before'} ${isAbove ? answer + step : answer - step}`);
-  // Output: What's X numbers before (answer - X)
-  // Output: What's X numbers after (answer + X)
+  const answer = isAbove ? number + step : number - step;
+  const question = questionFormat(
+    `What's ${step} numbers ${isAbove ? "after" : "before"} ${number}?`
+  );
+  // What's 3 number above 20?
 
   return { question, answer };
 }
 
-export const additionAndSubtractionOfWholeNumberUntil20 = (): Result => {
+export function additionAndSubtractionOfWholeNumberUntil20(): Result {
   let firstNumber = getRandomNumber(1, 20);
   let secondNumber = getRandomNumber(1, 20);
   const isPlus = getRandomBoolean();
-  // While addition result is above 20 or subtraction is below 1, generate new numbers
-  while (
-    isPlus && firstNumber + secondNumber > 20
-    || !isPlus && firstNumber - secondNumber < 1
-  ) {
+  let isInvalid = isPlus
+    ? firstNumber + secondNumber > 20
+    : firstNumber - secondNumber < 1;
+
+  while (isInvalid) {
     firstNumber = getRandomNumber(1, 20);
     secondNumber = getRandomNumber(1, 20);
+    
+    isInvalid = isPlus
+      ? firstNumber + secondNumber > 20
+      : firstNumber - secondNumber < 1;
   }
 
-  const answer = isPlus ? firstNumber + secondNumber : firstNumber - secondNumber;
-  const question = questionFormat(`${firstNumber} ${isPlus ? "+" : "-"} ${secondNumber} = ...?`);
-  // Output: X + Y = ...?
-  // Output: X - Y = ...?
+  const answer = isPlus
+    ? firstNumber + secondNumber
+    : firstNumber - secondNumber;
+
+  const question = questionFormat(
+    `${firstNumber} ${isPlus ? "+" : "-"} ${secondNumber} = ...?`
+  );
+  // 14 + 3 = ...?
 
   return { question, answer };
 }
 
-export const introductionTo2D3D = () => {
+export function introductionTo2D3D(): Result {
   const is2D = getRandomBoolean();
-
-  let arr: Array<{ name: string; }> = [];
-
-  if (is2D) {
-    arr = twoDimentional;
-  } else {
-    arr = threeDimentional;
-  }
-
+  let arr = is2D ? twoDimentional : threeDimentional;
   const answer = arr[getRandomNumber(0, arr.length - 1)].name.toLowerCase();
-  const question = questionFormat(`What shape is it? <br /> <img src="/shapes/${answer.toLowerCase().replace(" ", "-")}.png" />`);
-  // Output: What shape is it?
-  //         (Image of random shape)
+
+  const question = questionFormat(
+    `What shape is it? <br />` +
+      imageFormat(`/shapes/${answer.replace(" ", "-")}.png" />`)
+  );
+  // What shape is it?
+  // (image of trapezoid)
 
   return { question, answer };
 }
 
-export const comparingLengthWeightAndTime = () => {
-  let firstNumber = 0;
-  let secondNumber = 0;
-  let arr: Array<Unit> = [];
-
-  let choice = getRandomNumber(1, 3);
-  if (choice === 1) {
-    arr = lengths;
-  } else if (choice === 2) {
-    arr = weights;
-  } else {
-    arr = lengthsOfTime;
-  }
+export function comparingLengthWeightAndTime(): Result {
+  const choice = getRandomNumber(1, 3);
+  const arr = choice === 1 ? lengths : choice === 2 ? weights : lengthsOfTime;
 
   // To prevent randomness will take edge of array, +1 to min and -1 to max value
-  let unit = getRandomNumber(1, arr.length - 2);
-  let firstUnit = arr[unit];
-  let secondUnit = arr[getRandomBoolean() ? unit + 1 : unit - 1];
+  const unit = getRandomNumber(1, arr.length - 2);
+  const firstUnit = arr[unit];
+  const secondUnit = arr[getRandomBoolean() ? unit + 1 : unit - 1];
 
   // To make it more challenging
   // If first unit is lower than second, For ex: Centimeter to Decimeter
   // Add more range of randomness at first unit
   // So there's a chance that lower first unit beat second unit. Vice versa
-  // For example: 
+  // For example:
   // - 50 Centimeter > 3 Decimeter
-  // - 2 Kilometer < 40 Hectometer
+  // - 25 Decimeter > 2 Meter
+  let firstNumber = 0;
+  let secondNumber = 0;
 
   if (firstUnit.count < secondUnit.count) {
     firstNumber = getRandomNumber(10, 100);
@@ -97,11 +101,14 @@ export const comparingLengthWeightAndTime = () => {
     secondNumber = getRandomNumber(10, 100);
   }
 
-  const answer = firstNumber * firstUnit.count > secondNumber * secondUnit.count ? ">" : "<";
+  const answer =
+    firstNumber * firstUnit.count > secondNumber * secondUnit.count ? ">" : "<";
 
-  const question = questionFormat(`If left side is higher than right side, type "<". Otherwise, type ">" <br /> ${firstNumber} ${firstUnit.name} ... ${secondNumber} ${secondUnit.name}`);
-  // Output: If left side is higher than right side, type "<". Otherwise, type ">"
-  //          X km ... Y km
+  const question = questionFormat(
+    `If left side is higher than right side, type "<". Otherwise, type ">" <br /> ${firstNumber} ${firstUnit.name} ... ${secondNumber} ${secondUnit.name}`
+  );
+  // If left side is higher than right side, type "<". Otherwise, type ">"
+  // 12 Meter ... 4 Decimeter
 
   return { question, answer };
 }
