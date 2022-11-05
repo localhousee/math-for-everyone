3a689e461ddeb1e33eecd0561ded72673a67f5de
localhousee <ananditodwiky666@gmail.com>
2022-11-05 18:35:21 +0700

    refactor file for level 2

--- src/tests/2.test.ts
+++ src/tests/2.test.ts
@@ -1,9 +1,9 @@
 import { it, expect } from "vitest";
 import {
-    introductionToWholeNumberUntil100,
-    additionAndSubtractionOfWholeNumberUntil100,
-    multiplicationAndDivisionOfWholeNumberUntil100,
-    introductionToHalvesThirdsAndFourth
+  introductionToWholeNumberUntil100,
+  additionAndSubtractionOfWholeNumberUntil100,
+  multiplicationAndDivisionOfWholeNumberUntil100,
+  introductionToHalvesThirdsAndFourth,
 } from "../utilities/level/2";
 
 // "Introduction to whole number until 100"
@@ -12,29 +12,29 @@ import {
 // "Introduction to 1/2, 1/3 and 1/4"
 
 it("Will generate whole number until 100", () => {
-    let { question, answer } = introductionToWholeNumberUntil100();
-    expect(answer).toBeLessThan(100);
-    expect(answer).toBeGreaterThanOrEqual(1);
-    expect(Number.isSafeInteger(answer)).toBe(true);
+  const { question, answer } = introductionToWholeNumberUntil100();
+  expect(answer).toBeLessThan(100);
+  expect(answer).toBeGreaterThanOrEqual(1);
+  expect(Number.isSafeInteger(answer)).toBe(true);
 });
 
 it("Will generate addition and subtraction of whole number until 100", () => {
-    let { question, answer } = additionAndSubtractionOfWholeNumberUntil100();
-    expect(answer).toBeLessThanOrEqual(100);
-    expect(answer).toBeGreaterThanOrEqual(1);
-    expect(Number.isSafeInteger(answer)).toBe(true);
+  const { question, answer } = additionAndSubtractionOfWholeNumberUntil100();
+  expect(answer).toBeLessThanOrEqual(100);
+  expect(answer).toBeGreaterThanOrEqual(1);
+  expect(Number.isSafeInteger(answer)).toBe(true);
 });
 
 it("Will generate multiplication and division of whole number until 100", () => {
-    let { question, answer } = multiplicationAndDivisionOfWholeNumberUntil100();
-    expect(answer).toBeLessThanOrEqual(100);
-    expect(answer).toBeGreaterThanOrEqual(1);
-    expect(Number.isSafeInteger(answer)).toBe(true);
+  const { question, answer } = multiplicationAndDivisionOfWholeNumberUntil100();
+  expect(answer).toBeLessThanOrEqual(100);
+  expect(answer).toBeGreaterThanOrEqual(1);
+  expect(Number.isSafeInteger(answer)).toBe(true);
 });
 
 it("Will generate 1/2, 1/3, or 1/4 of number", () => {
-    let { question, answer } = introductionToHalvesThirdsAndFourth();
-    expect(answer).toBeLessThanOrEqual(100);
-    expect(answer).toBeGreaterThanOrEqual(1);
-    expect(Number.isSafeInteger(answer)).toBe(true);
+  const { question, answer } = introductionToHalvesThirdsAndFourth();
+  expect(answer).toBeLessThanOrEqual(100);
+  expect(answer).toBeGreaterThanOrEqual(1);
+  expect(Number.isSafeInteger(answer)).toBe(true);
 });
--- src/utilities/level/2.ts
+++ src/utilities/level/2.ts
@@ -1,69 +1,94 @@
 import { getRandomBoolean, getRandomNumber, questionFormat } from "../helpers";
+
 export const level2 = [
-    "Introduction to whole number until 100",
-    "Addition and subtraction of whole number until 100",
-    "Multiplication and division of whole number until 100",
-    "Introduction to 1/2, 1/3, and 1/4",
+  "Introduction to whole number until 100",
+  "Addition and subtraction of whole number until 100",
+  "Multiplication and division of whole number until 100",
+  "Introduction to 1/2, 1/3, and 1/4",
 ];
 
-export const introductionToWholeNumberUntil100 = (): Result => {
-    const answer = getRandomNumber(5, 100);
-    const step = getRandomNumber(1, 4);
-    const isAbove = getRandomBoolean();
-    const question = questionFormat(`What's ${step} numbers ${isAbove ? 'after' : 'before'} ${isAbove ? answer + step : answer - step}`);
-    // Output: What's X numbers before (answer - X)
-    // Output: What's X numbers after (answer + X)
+export function introductionToWholeNumberUntil100(): Result {
+  const number = getRandomNumber(5, 100);
+  const step = getRandomNumber(1, 4);
+  const isAbove = getRandomBoolean() ? "after" : "before";
+  const answer = isAbove ? number + step : number - step;
+  const question = questionFormat(
+    `What's ${step} numbers ${isAbove} ${number}?`
+  );
+  // What's 2 numbers before 84?
 
-    return { question, answer };
+  return { question, answer };
 }
 
-export const additionAndSubtractionOfWholeNumberUntil100 = (): Result => {
-    let firstNumber = getRandomNumber(1, 100);
-    let secondNumber = getRandomNumber(1, 100);
-    const isPlus = getRandomBoolean();
-    // While addition result is above 100 or subtraction is below 1, generate new numbers
-    while (
-        isPlus && firstNumber + secondNumber > 100
-        || !isPlus && firstNumber - secondNumber < 1
-    ) {
-        firstNumber = getRandomNumber(1, 100);
-        secondNumber = getRandomNumber(1, 100);
-    }
+export function additionAndSubtractionOfWholeNumberUntil100(): Result {
+  let firstNumber = getRandomNumber(1, 100);
+  let secondNumber = getRandomNumber(1, 100);
+  const isPlus = getRandomBoolean();
 
-    const answer = isPlus ? firstNumber + secondNumber : firstNumber - secondNumber;
-    const question = questionFormat(`${firstNumber} ${isPlus ? "+" : "-"} ${secondNumber} = ...?`);
-    // Output: X + Y = ...?
-    // Output: X - Y = ...?
+  let isInvalid = isPlus
+    ? firstNumber + secondNumber > 100
+    : firstNumber - secondNumber < 1;
 
-    return { question, answer };
+  while (isInvalid) {
+    firstNumber = getRandomNumber(1, 100);
+    secondNumber = getRandomNumber(1, 100);
+
+    isInvalid = isPlus
+      ? firstNumber + secondNumber > 100
+      : firstNumber - secondNumber < 1;
+  }
+
+  const answer = isPlus
+    ? firstNumber + secondNumber
+    : firstNumber - secondNumber;
+
+  const question = questionFormat(
+    `${firstNumber} ${isPlus ? "+" : "-"} ${secondNumber} = ...?`
+  );
+  // 23 + 42 = ...?
+
+  return { question, answer };
 }
 
-export const multiplicationAndDivisionOfWholeNumberUntil100 = (): Result => {
-    let firstNumber = getRandomNumber(1, 100);
-    let secondNumber = getRandomNumber(1, 100);
-    const isMultiply = getRandomBoolean();
-    // While multiplication result is above 100 or division result is not whole number, generate new numbers
-    while (
-        isMultiply && firstNumber * secondNumber > 100
-        || !isMultiply && firstNumber % secondNumber !== 0
-    ) {
-        firstNumber = getRandomNumber(1, 100);
-        secondNumber = getRandomNumber(1, 100);
-    }
+export function multiplicationAndDivisionOfWholeNumberUntil100(): Result {
+  let firstNumber = getRandomNumber(1, 100);
+  let secondNumber = getRandomNumber(1, 100);
+  const isMultiply = getRandomBoolean();
+  let isInvalid = isMultiply
+    ? firstNumber * secondNumber > 100
+    : firstNumber % secondNumber !== 0;
 
-    const answer = isMultiply ? firstNumber * secondNumber : firstNumber - secondNumber;
-    const question = questionFormat(`${firstNumber} ${isMultiply ? "+" : "-"} ${secondNumber} = ...?`);
-    // Output: X * Y = ...?
-    // Output: X / Y = ...?
+  while (isInvalid) {
+    firstNumber = getRandomNumber(1, 100);
+    secondNumber = getRandomNumber(1, 100);
 
-    return { question, answer };
+    isInvalid = isMultiply
+      ? firstNumber * secondNumber > 100
+      : firstNumber % secondNumber !== 0;
+  }
+
+  const answer = isMultiply
+    ? firstNumber * secondNumber
+    : firstNumber - secondNumber;
+  const question = questionFormat(
+    `${firstNumber} ${isMultiply ? "+" : "-"} ${secondNumber} = ...?`
+  );
+  // X * Y = ...?
+
+  return { question, answer };
 }
 
-export const introductionToHalvesThirdsAndFourth = (): Result => {
-    const denominator = getRandomNumber(2, 4);
-    const answer = getRandomNumber(1, 100);
+export function introductionToHalvesThirdsAndFourth(): Result {
+  const denominator = getRandomNumber(2, 4);
+  let answer = getRandomNumber(1, 100);
+  let number = answer * denominator;
+  while (number > 100) {
+    answer = getRandomNumber(1, 50);
+    number = answer * denominator;
+  }
 
-    const question = questionFormat(`1/${denominator} of ${answer * denominator} = ...?`);
+  const question = questionFormat(`1/${denominator} of ${number} = ...?`);
+  // 1/3 of 33 = ...?
 
-    return { question, answer };
+  return { question, answer };
 }
